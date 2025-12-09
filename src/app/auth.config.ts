8c3607705161303e9aa596abf9e06ecbe24497e4
nourredin <nourredin@gmail.com>
2025-12-10 00:26:19 +0100

    refactor: update component imports and fix redirect URI in auth configate

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -3,7 +3,7 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 export const authConfig: AuthConfig = {
   issuer: 'https://accounts.google.com', // Google OAuth endpoint
   clientId: '619976883052-gqapne5du344dm7g3ch5r08vbqh31p72.apps.googleusercontent.com', // Replace with actual Client ID
-  redirectUri: window.location.origin,
+  redirectUri: 'http://localhost:4200',
   scope: 'openid profile email',
   strictDiscoveryDocumentValidation: false,
 };
